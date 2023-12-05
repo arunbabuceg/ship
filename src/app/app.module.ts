db2a4e7e328fe374a5f9c481acbcd9ba63330591
arunbabuceg <150684927+arunbabuceg@users.noreply.github.com>
2023-12-05 11:04:17 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { CoheroComponent } from './cohero/cohero.component';
 import { CoreComponent } from './core/core.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PeopleComponent } from './people/people.component';
+import { HeroineComponent } from './heroine/heroine.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { CoreComponent } from './core/core.component';
     HeaderComponent,
     HeroComponent,
     CoheroComponent,
-    CoreComponent
+    CoreComponent,
+    GalleryComponent,
+    PeopleComponent,
+    HeroineComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
